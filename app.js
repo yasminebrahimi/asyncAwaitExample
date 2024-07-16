bdf97add5d9901d91b45a91df9eae71ec8a1d455
Yasmin Ebrahimi <yasminebrahimi54@gmail.com>
2024-07-16 23:02:41 +0300

    Comments added

--- app.js
+++ app.js
@@ -1,39 +1,38 @@
 
 
-
-
-
-
-
-
-function loginUser (email, password){
-    return new Promise ((resolve, reject) => {
+// Function to simulate logging in a user with email and password
+function loginUser(email, password) {
+    return new Promise((resolve, reject) => {
+        // Simulating asynchronous operation with setTimeout
         setTimeout(() => {
-            resolve({email}); 
-        }, 2000); 
-    }); 
+            resolve({ email }); // Resolving with user's email
+        }, 2000); // Simulating 2 seconds delay
+    });
 }
 
-function findEnrolledCourses (email){
-    return new Promise ((resolve, reject) => {
+// Function to simulate fetching enrolled courses based on user's email
+function findEnrolledCourses(email) {
+    return new Promise((resolve, reject) => {
+        // Simulating asynchronous operation with setTimeout
         setTimeout(() => {
-            resolve(["React.js", "Next.js", "Node.js"]); 
-        }, 1000); 
-    }); 
+            resolve(["React.js", "Next.js", "Node.js"]); // Resolving with array of course titles
+        }, 1000); // Simulating 1 second delay
+    });
 }
 
-function getEpisodes (courseTitle){
-    return new Promise ((resolve, reject) => {
+// Function to simulate fetching episodes of a course based on course title
+function getEpisodes(courseTitle) {
+    return new Promise((resolve, reject) => {
+        // Simulating asynchronous operation with setTimeout
         setTimeout(() => {
-            resolve(["E01", "E02", "E03"]); 
-        }, 2000); 
-    }); 
+            resolve(["E01", "E02", "E03"]); // Resolving with array of episode names
+        }, 2000); // Simulating 2 seconds delay
+    });
 }
 
-
+// Example usage of the above functions chained together
 loginUser("user@gmail.com")
-.then((userData) => findEnrolledCourses(userData.email))
-.then((courses) =>  getEpisodes(courses[0]))
-.then((episodes) => console.log(episodes))
-.catch((err) => console.log(err.message)); 
-
+    .then((userData) => findEnrolledCourses(userData.email)) // Fetch enrolled courses after logging in
+    .then((courses) => getEpisodes(courses[0])) // Fetch episodes of the first enrolled course
+    .then((episodes) => console.log(episodes)) // Log episodes to console
+    .catch((err) => console.log(err.message)); // Catch and log any errors during the process
